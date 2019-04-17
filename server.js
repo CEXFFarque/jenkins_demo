0819b11e3747ca970cac69f76451655e96f2ac18
Fahim Farque <ffarque@cec000277.local>
2019-04-17 13:43:12 -0400

    index.html update

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const server = express();
 const PORT = 80;
 
 server.listen(PORT, () => console.log(`Sever running on ${PORT}`));
-server.get('/', (req, res) => res.status(200).send('hello world!'));
+server.get('/', (req, res) => res.status(200).send('hello Devcon!'));
