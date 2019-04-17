9bd7c5d6575088bde38e1c6563a87f4d69ddf648
Fahim Farque <ffarque@cec000277.local>
2019-04-16 23:46:50 -0400

    initial push

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const server = express();
+const PORT = 80;
+server.listen(PORT, () => console.log(`Sever running on ${PORT}`));
+server.get('/', (req, res) => res.status(200).send('hello'));
