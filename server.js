75cf054008c342a71117f0c3bb333b9cf23f087f
Fahim Farque <ffarque@cec000277.local>
2019-04-17 10:20:20 -0400

    index.html update

--- server.js
+++ server.js
@@ -2,10 +2,5 @@ const express = require('express');
 const server = express();
 const PORT = 80;
 
-server.get('/', function(req, res) {
-    res.sendFile('index.html');
-});
-
 server.listen(PORT, () => console.log(`Sever running on ${PORT}`));
-
-//server.get('/', (req, res) => res.status(200).send('hello world'));
+server.get('/', (req, res) => res.status(200).send('hello world'));
